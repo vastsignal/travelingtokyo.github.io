443d68a89ed24922992ad763fb1262a36ac28ba4
vastsignal <fk6c7nzbrn@privaterelay.appleid.com>
2025-11-23 23:51:38 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/my-travel-app/", 
 })
 
-base: "/my-travel-app/"
